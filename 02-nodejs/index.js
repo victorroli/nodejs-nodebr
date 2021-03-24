442376d8e78b06829abdafade68843b6e28ae7ef
Victor Felipe <victorfelipe1608@gmail.com>
2021-03-23 21:06:37 -0300

    Working with promises and callbacks

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -4,60 +4,105 @@
  * Obter o endereço do usuário pelo Id
  * 
  */
+const util = require('util')
+const obterEnderecoAsync = util.promisify(obterEndereco)
 
-function obterUsuario(callback){
-    setTimeout(function() {
-        return callback(null, {
-            id: 1,
-            nome: 'Client Test',
-            dataNascimento: new Date()
-        });
-    }, 2000)
+function obterUsuario(){
+
+    return new Promise(function resolvePromise(resolve, reject ){
+        
+        setTimeout(function() {
+            return resolve({
+                id: 1,
+                nome: 'Client Test',
+                dataNascimento: new Date()
+            });
+        }, 2000)
+
+    })
+
 }
 
-function obterTelefone(idUsuario, callback){
-    setTimeout(function(){
-        return callback(null, {
-            telefone: '999999999',
-            ddd: 38
-        })
-    }, 2000)
+function obterTelefone(idUsuario){
+
+    return new Promise(function resolverPromise(resolve, reject){
+
+        setTimeout(function(){
+            return resolve({
+                telefone: '999999999',
+                ddd: 38
+            })
+        }, 2000)
+
+    })
 }
 
 function obterEndereco(idUsuario, callback){
-    setTimeout(function(){
-        return callback(null, {
-            rua: 'Rua Dos bobos',
-            numero: 0
-        })
-    }, 2000)
+    
+        setTimeout(function(){
+            return callback(null, {
+                rua: 'Rua Dos bobos',
+                numero: 0
+            })
+        }, 2000)
+
 }
 
-obterUsuario(function resolverUsuario(error, usuario){
-    if(error){
-        console.error('Erro ao buscar Usuário', error)
-        return
-    }
+// const usuarioPromise = obterUsuario()
+// usuarioPromise
+//     .then(function(usuario){
+//         return obterTelefone(usuario.id)
+//             .then(function (result){
+//                 return {
+//                     usuario: {
+//                         nome: usuario.nome,
+//                         id: usuario.id
+//                     },
+//                     telefone: result
+//                 }
+//             })
+//     })
+//     .then(function(resultado){
+//         const endereco = obterEnderecoAsync(resultado.usuario.id)
+//         return endereco.then(function(result){
+//             return {
+//                 usuario: resultado.usuario,
+//                 telefone: resultado.telefone,
+//                 endereco: result
+//             }
+//         })
+//     })
+//     .then(function(resultado){
+//         console.log('Resultado obtido: ', resultado)
+//     }).catch(function(error){
+//         console.log('Erro pego: ', error)
+//     })
 
-    obterTelefone(usuario.id, function resolverTelefone(error1, telefone){
-        if(error1){
-            console.error('Erro ao buscar telefone', error1)
-            return
-        }
+// obterUsuario(function resolverUsuario(error, usuario){
+//     if(error){
+//         console.error('Erro ao buscar Usuário', error)
+//         return
+//     }
+
+//     obterTelefone(usuario.id, function resolverTelefone(error1, telefone){
+//         if(error1){
+//             console.error('Erro ao buscar telefone', error1)
+//             return
+//         }
         
-        obterEndereco(usuario.id, function resolverEndereco(error2, endereco){
-            if(error2){
-                console.log('Erro ao buscar endereço', error2);
-                return
-            }
+//         obterEndereco(usuario.id, function resolverEndereco(error2, endereco){
+//             if(error2){
+//                 console.log('Erro ao buscar endereço', error2);
+//                 return
+//             }
             
-            console.log(`
-                Nome: ${usuario.nome},
-                Endereço: ${endereco.rua}, nº ${endereco.numero},
-                Telefone: (${telefone.ddd}) ${telefone.telefone}
-            `);
-        })
+            // console.log(`
+            //     Nome: ${usuario.nome},
+            //     Endereço: ${endereco.rua}, nº ${endereco.numero},
+            //     Telefone: (${telefone.ddd}) ${telefone.telefone}
+            // `);
+//         })
 
-    })
-});
+//     })
+// });
 
