c222682bdc2ac784317aea402a50460ea37c4caa
Victor Oliveira <victorfelipe1608@gmail.com>
2022-11-29 15:59:33 -0300

    Manipulating files - DELETE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -30,4 +30,11 @@ describe('Suite de manipulação de Herois', () => {
         
         deepEqual(actual, expected)
     })
+
+    it('deve remover um heroi pelo id', async() => {
+        const expected = true
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+
+        deepEqual(resultado, expected)
+    })
 })
